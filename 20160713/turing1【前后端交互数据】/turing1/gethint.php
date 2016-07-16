e09c61f43bc7287261ce98a5248d80582dfe8a8d
2587575267@qq.com <2587575267@qq.com>
2016-07-16 17:55:34 +0800

    更改0713的文件

---
+++ 20160713/turing1【前后端交互数据】/turing1/gethint.php
@@ -0,0 +1,11 @@
+<?php
+$getInput = $_GET["input"];
+//	if($getInput=="姚老师"){
+//		echo $getInput."你好傻啊";
+//	}else{
+//		echo $getInput."你好聪明噢";
+//	}
+//sql
+$getInput++;
+echo $getInput . "你好聪明噢";
+?>
