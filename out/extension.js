7dece2ed442d47c28ab9199f70ea5e2728f40fdd
enoyao <enoyao@tencent.com>
2020-05-23 18:08:02 +0800

    test
    test

--- out/extension.js
+++ out/extension.js
@@ -9,6 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
 const fs = require("fs");
 const p = require("path");
