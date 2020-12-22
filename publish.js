0ed77d08f4fad9e24409cca8b4f8c60986f3d8da
wscats <kalone.cool@gmail.com>
2020-12-22 08:06:58 +0800

    add badge
    add badge

--- publish.js
+++ publish.js
@@ -18,9 +18,14 @@ const packageJson = {
     },
     "badges": [
         {
-            "url": "https://img.shields.io/badge/vscode--dev--community-compilehero-blue.svg?logo=slack&labelColor=555555",
-            "href": "https://vscode-slack.amod.io",
-            "description": "Join us in the #compilehero channel"
+            "url": "https://img.shields.io/badge/Author-Eno Yao-blueviolet",
+            "href": "https://github.com/Wscats",
+            "description": "Eno Yao"
+        },
+        {
+            "url": "https://vsmarketplacebadge.apphb.com/version-short/wscats.eno.svg?color=blue&style=flat-square",
+            "href": "https://marketplace.visualstudio.com/items?itemName=Wscats.eno",
+            "description": "VS Marketplace"
         }
     ],
     "galleryBanner": {
@@ -524,7 +529,7 @@ switch (process.argv[2]) {
         packageJson.name = "qf";
         packageJson.displayName = "Formatter Hero - Beautify Sass/Less/Scss/Typescript/Javascript/Jade/Pug";
         packageJson.description = "🧣Beautify sass, less, scss, typescript, javascript, jade and pug.";
-        packageJson.version = "6.8.113";
+        packageJson.version = "6.8.114";
         packageJson.preview = true;
         packageJson.icon = "logos/hero4.png";
         packageJson.contributes.configuration.properties["compile-hero.disable-compile-files-on-did-save-code"] = {
@@ -569,7 +574,7 @@ switch (process.argv[2]) {
             "default": true,
             "description": "Disable compile files on did save code."
         };
-        packageJson.version = "2.3.44";
+        packageJson.version = "2.3.45";
         packageJson.preview = true;
         packageJson.icon = "logos/hero2.png";
         fs.writeFileSync('./package.json', JSON.stringify(packageJson));
