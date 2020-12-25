60dd77f6d6ba0a447599b50b781496b43449b811
wscats <kalone.cool@gmail.com>
2020-12-25 15:50:17 +0800

    更新启动的流程
    更新启动的流程

--- publish.js
+++ publish.js
@@ -80,15 +80,16 @@ const packageJson = {
         "formate"
     ],
     "activationEvents": [
-        "onLanguage:javascript",
         "onLanguage:less",
         "onLanguage:scss",
         "onLanguage:sass",
         "onLanguage:jade",
         "onLanguage:pug",
         "onLanguage:stylus",
+        "onLanguage:javascript",
+        "onLanguage:javascriptreact",
         "onLanguage:typescript",
-        "onLanguage:typescriptx",
+        "onLanguage:typescriptreact",
         "onCommand:compile-hero.openInBrowser",
         "onCommand:compile-hero.closePort",
         "onCommand:compile-hero.compileFile",
@@ -552,7 +553,7 @@ switch (process.argv[2]) {
         packageJson.name = "qf";
         packageJson.displayName = "Formatter Hero - Beautify Sass/Less/Scss/Typescript/Javascript/Jade/Pug";
         packageJson.description = "🧣Beautify sass, less, scss, typescript, javascript, jade and pug.";
-        packageJson.version = "6.8.117";
+        packageJson.version = "6.8.119";
         packageJson.preview = true;
         packageJson.icon = "logos/hero4.png";
         packageJson.contributes.configuration.properties["compile-hero.disable-compile-files-on-did-save-code"] = {
@@ -597,7 +598,7 @@ switch (process.argv[2]) {
             "default": true,
             "description": "Disable compile files on did save code."
         };
-        packageJson.version = "2.3.48";
+        packageJson.version = "2.3.50";
         packageJson.preview = true;
         packageJson.icon = "logos/hero2.png";
         fs.writeFileSync('./package.json', JSON.stringify(packageJson));
