74432ed827ac27bcc515d249d5109ea316562b01
priyanka447 <90445422+priyanka447@users.noreply.github.com>
2021-09-15 12:00:49 +0530

    priyanka447

---
+++ Title.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Titles = () => (
+
+    <div>
+    <h1 className="title-container__title">Weather Finder</h1>
+    <h3 className="title-container__subtitle">Find out temperature,conditions and more...</h3>
+    </div>
+    );
+
+
+    export default Titles;
